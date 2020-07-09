7dc6e9902722f68cce099c763faa74d9e1c8f02e
vladimirpotanin <vladimir971001@yandex.ru>
2020-07-09 07:04:21 +0200

    'finishedFrontend'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,18 @@ import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
 import { AgmCoreModule } from '@agm/core';
 import { EventComponent } from './event/event.component';
+import { AngularFireModule } from '@angular/fire';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCZx6hTBKw-xTlhIpmY4MbfplMYDGkFSDA",
+  authDomain: "runtrip-ar.firebaseapp.com",
+  databaseURL: "https://runtrip-ar.firebaseio.com",
+  projectId: "runtrip-ar",
+  storageBucket: "runtrip-ar.appspot.com",
+  messagingSenderId: "654028737360",
+  appId: "1:654028737360:web:acee98dca94c09a8553a55",
+  measurementId: "G-YD5S2HS061"
+};
 
 @NgModule({
   declarations: [
@@ -37,11 +49,13 @@ import { EventComponent } from './event/event.component';
     FormsModule,
     RouterModule,
     AppRoutingModule,
-    HomeModule,
+    HomeModule,  
+    AngularFireModule.initializeApp(firebaseConfig),  
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCjll58FtHCBljBY2x5-yXqSi6UdID1Gng'
     })
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
